7b6f4aa2a2ed325de7c9cd8c713c3b64e029e5dd
Vivi <a370109945@gmail.com>
2022-09-16 01:11:55 +0800

    Update publish script
    - Add `testnet`
    - Remove proxy
    - Add Hippo coins for test

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,28 +2,27 @@
 # Copyright (c) 2022, Vivid Network Contributors
 # SPDX-License-Identifier: Apache-2.0
 
+HIPPO_DEVNET_TOKEN_PACKAGE="0x498d8926f16eb9ca90cab1b3a26aa6f97a080b3fcbe6e83ae150b7243a00fb68"
+
 LOCALTEST_FULLNODE_URL="http://localhost:8080/v1"
 LOCALTEST_FAUCET_URL="http://localhost:8081"
 
 DEVNET_FULLNODE_URL="https://fullnode.devnet.aptoslabs.com/v1"
 DEVNET_FAUCET_URL="https://faucet.devnet.aptoslabs.com/"
 
+TESTNET_FULLNODE_URL="https://ait3.aptosdev.com/v1"
+TESTNET_FAUCET_URL=""
+
 FULLNODE_URL="${DEVNET_FULLNODE_URL}"
 FAUCET_URL="${DEVNET_FAUCET_URL}"
-
-REST_PREFIX="";
-if [ -x "$(command -v proxychains4)" ]; then
-    REST_PREFIX="proxychains4 -q"
-    echo "Using proxychains4 for proxy"
-elif [ -x "$(command -v proxychains)" ]; then
-    REST_PREFIX="proxychains -q"
-    echo "Using proxychains for proxy"
-fi
 
 if [[ ${APTOSWAP_PUBLISH_NETWORK} == "devnet" ]]; then
     FULLNODE_URL="${DEVNET_FULLNODE_URL}"
     FAUCET_URL="${DEVNET_FAUCET_URL}"
-else
+elif [[ ${APTOSWAP_PUBLISH_NETWORK} == "testnet" ]]; then
+    FULLNODE_URL="${TESTNET_FULLNODE_URL}"
+    FAUCET_URL="${TESTNET_FAUCET_URL}"
+elif [[ ${APTOSWAP_PUBLISH_NETWORK} == "localhost" ]]; then
     FULLNODE_URL="${LOCALTEST_FULLNODE_URL}"
     FAUCET_URL="${LOCALTEST_FAUCET_URL}"
 fi
@@ -59,9 +58,34 @@ echo "[3] Publish..."
 ${REST_PREFIX} aptos move publish --named-addresses Aptoswap=default --url=${FULLNODE_URL} --max-gas 200000
 echo "[4] Initilaize..."
 ${REST_PREFIX} aptos move run --function-id default::pool::initialize --args u8:6 --url=${FULLNODE_URL}
-echo "[5] Create pool..."
-${REST_PREFIX} aptos move run --function-id default::pool::create_pool --type-args ${APTOSWAP_PACKAGE_ADDR}::pool::TestToken 0x1::aptos_coin::AptosCoin --args u64:5 u64:25 --url ${FULLNODE_URL}
-echo "[6] Create APTS/APT..."
-${REST_PREFIX} aptos move run --function-id default::pool::create_pool --type-args ${APTOSWAP_PACKAGE_ADDR}::pool::Token 0x1::aptos_coin::AptosCoin --args u64:5 u64:25 --url ${FULLNODE_URL}
-echo "[7] Create Minting APTS..."
-${REST_PREFIX} aptos move run --function-id default::pool::mint_token --args u64:10000000000 address:${APTOSWAP_PACKAGE_ADDR} --url ${FULLNODE_URL}
+echo "[5] Create pools..."
+
+echo "    Create pool[${APTOSWAP_PACKAGE_ADDR}::pool::TestToken / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${APTOSWAP_PACKAGE_ADDR}::pool::TestToken 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "    Create pool[${APTOSWAP_PACKAGE_ADDR}::pool::Token / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${APTOSWAP_PACKAGE_ADDR}::pool::Token 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "    Create pool[${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetBNB / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetBNB 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "    Create pool[${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetBTC / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetBTC 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "    Create pool[${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetDAI / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetDAI 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "    Create pool[${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetETH / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetETH 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "    Create pool[${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetSOL / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetSOL 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "    Create pool[${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetUSDC / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetUSDC 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "    Create pool[${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetUSDT / 0x1::aptos_coin::AptosCoin]"
+aptos move run --function-id default::pool::create_pool --type-args ${HIPPO_DEVNET_TOKEN_PACKAGE}::devnet_coins::DevnetUSDT 0x1::aptos_coin::AptosCoin --args u64:4 u64:26 --url ${FULLNODE_URL}
+
+echo "[6] Create Minting APTS..."
+aptos move run --function-id default::pool::mint_token --args u64:10000000000 address:${APTOSWAP_PACKAGE_ADDR} --url ${FULLNODE_URL}
